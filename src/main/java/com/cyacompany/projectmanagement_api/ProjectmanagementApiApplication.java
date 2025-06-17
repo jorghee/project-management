0a1faaa4e75975aca7625d76797ac9791684d89e
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-17 00:16:29 -0500

    Create Spring Boot project

---
+++ src/main/java/com/cyacompany/projectmanagement_api/ProjectmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyacompany.projectmanagement_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmanagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyacompany/projectmanagement_api/ProjectmanagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyacompany.projectmanagement_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
