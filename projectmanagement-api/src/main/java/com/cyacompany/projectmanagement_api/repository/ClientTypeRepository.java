3ae4ea6d6315e782aea1f55cf6cb80c1e490273d
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-24 14:39:55 -0500

    Add findAllByOrderByIdAsc method to sort our output

--- projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/repository/ClientTypeRepository.java
+++ projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/repository/ClientTypeRepository.java
@@ -3,5 +3,8 @@ package com.cyacompany.projectmanagement_api.repository;
 import com.cyacompany.projectmanagement_api.model.ClientType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClientTypeRepository extends JpaRepository<ClientType, Integer> {
+  List<ClientType> findAllByOrderByIdAsc();
 }
