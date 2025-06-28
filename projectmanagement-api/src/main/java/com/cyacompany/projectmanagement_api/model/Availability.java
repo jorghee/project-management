b62c9b88e67d9caa9298460bcc6e2fc09a2597de
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-28 00:38:58 -0500

    Ad the two new fields as DisCapSem and DisCarAct

--- projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/model/Availability.java
+++ projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/model/Availability.java
@@ -12,16 +12,22 @@ import lombok.*;
 public class Availability {
 
   @Id
-  @Column(name = "EmpCod")
+  @Column(name = "DisEmpCod")
   private Integer employeeId;
 
   @OneToOne
   @MapsId
-  @JoinColumn(name = "EmpCod")
+  @JoinColumn(name = "DisEmpCod")
   private Employee employee;
 
   @Column(name = "DisEst", length = 1, nullable = false)
   private String availabilityStatus;
+
+  @Column(name = "DisCapSem")
+  private Integer weeklyCapacity;
+
+  @Column(name = "DisCarAct")
+  private Integer currentLoad;
 
   @Column(name = "EstReg", length = 1, nullable = false)
   private String status;
