a7e8cb5e917cdf0585fd1848ed43f649a130bd76
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-29 18:22:15 -0500

    Add findByProjectIdOrderByIdAsc and existsByProjectId methods

--- projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/repository/StageRepository.java
+++ projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/repository/StageRepository.java
@@ -3,4 +3,16 @@ package com.cyacompany.projectmanagement_api.repository;
 import com.cyacompany.projectmanagement_api.model.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StageRepository extends JpaRepository<Stage, Integer> {}
+import java.util.List;
+
+public interface StageRepository extends JpaRepository<Stage, Integer> {
+
+  /**
+   * Encuentra todas las etapas de un proyecto específico, ordenadas por su ID.
+   * @param projectId El ID del proyecto.
+   * @return Una lista de etapas.
+   */
+  List<Stage> findByProjectIdOrderByIdAsc(Integer projectId);
+
+  boolean existsByProjectId(Integer projectId); 
+}
