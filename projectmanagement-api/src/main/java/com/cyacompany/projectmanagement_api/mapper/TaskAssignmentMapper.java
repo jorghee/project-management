c3afefbe6b743497e5340452b8bc17441e5b0a8b
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-29 23:18:27 -0500

    Add TaskAssignment mapper to TaskAssigment controller

---
+++ projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/mapper/TaskAssignmentMapper.java
@@ -0,0 +1,18 @@
+package com.cyacompany.projectmanagement_api.mapper;
+
+import com.cyacompany.projectmanagement_api.dto.TaskAssignmentRequest;
+import com.cyacompany.projectmanagement_api.dto.TaskAssignmentResponse;
+import com.cyacompany.projectmanagement_api.model.TaskAssignment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TaskAssignmentMapper {
+  @Mapping(source = "employee.id", target = "employeeId")
+  @Mapping(source = "employee.name", target = "employeeName")
+  @Mapping(source = "task.id", target = "taskId")
+  @Mapping(source = "task.description", target = "taskDescription")
+  TaskAssignmentResponse toResponse(TaskAssignment assignment);
+
+  TaskAssignment toEntity(TaskAssignmentRequest request);
+}
