8f4dd992eba251963658f677fc5145c0b2c0b90d
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-29 23:20:42 -0500

    Add TaskExecution mapper to TaskExecution controller

---
+++ projectmanagement-api/src/main/java/com/cyacompany/projectmanagement_api/mapper/TaskExecutionMapper.java
@@ -0,0 +1,15 @@
+package com.cyacompany.projectmanagement_api.mapper;
+
+import com.cyacompany.projectmanagement_api.dto.TaskExecutionRequest;
+import com.cyacompany.projectmanagement_api.dto.TaskExecutionResponse;
+import com.cyacompany.projectmanagement_api.model.TaskExecution;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TaskExecutionMapper {
+  @Mapping(source = "assignment.id", target = "assignmentId")
+  TaskExecutionResponse toResponse(TaskExecution execution);
+  
+  TaskExecution toEntity(TaskExecutionRequest request);
+}
